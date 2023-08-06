230698395e490d8773218fb2f82d98da46ab8e66
Vetal <heavyve@gmail.com>
2023-08-06 09:54:40 +0300

    Parsing закончили реализацию метода parse добавлен метод mapToList

--- src/main/java/Parsing.java
+++ src/main/java/Parsing.java
@@ -1,6 +1,7 @@
 import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -13,10 +14,34 @@ public class Parsing {
         String pathEncrypted = Util.readerString();
         Util.writeMessage("Введите путь к файлу статистики");
         String pathStatistic = Util.readerString();
+        Path path = Util.builderFileName(pathEncrypted, "_pars");
         Map<Character, Integer> mapEncrypted = getFillMap(pathEncrypted);
         Map<Character, Integer> mapStatic = getFillMap(pathStatistic);
-        Set<Map.Entry<Character, Integer>> entries = mapEncrypted.entrySet();
-        ArrayList<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
+        List<Map.Entry<Character, Integer>> listEncrypted = mapToList(mapEncrypted);
+        List<Map.Entry<Character, Integer>> listStatic = mapToList(mapStatic);
+        Map<Character, Character> decrypted = new HashMap<>();
+        if (listEncrypted.size() <= listStatic.size()) {
+            for (int i = 0; i < listEncrypted.size(); i++) {
+                decrypted.put(listEncrypted.get(i).getKey(), listStatic.get(i).getKey());
+            }
+        } else {
+            Util.writeMessage("Размер файла статистики недостаточен, нужен файл большей размерности");
+        }
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(pathEncrypted));
+             BufferedWriter writer = Files.newBufferedWriter(path)) {
+
+            while (reader.ready()) {
+                StringBuilder builder = new StringBuilder();
+                String line = reader.readLine();
+                for (int i = 0; i < line.length(); i++) {
+                    Character ch = decrypted.get(line.charAt(i));
+                    builder.append(ch);
+                }
+                writer.write(builder.toString() + System.lineSeparator());
+            }
+
+        }
+        Util.writeMessage("Файл расшиврован и находится по адресу " + path);
 
     }
 
@@ -30,19 +55,21 @@ public class Parsing {
                 builder.append(reader.readLine()).append(System.lineSeparator());
             }
             for (char aChar : builder.toString().toCharArray()) {
-                if (!map.containsKey(aChar)) {
-                    map.put(aChar, 1);
-                } else {
-                    map.put(aChar, map.get(aChar) + 1);
-                }
+//                if (!map.containsKey(aChar)) {
+//                    map.put(aChar, 1);
+//                } else {
+//                    map.put(aChar, map.get(aChar) + 1);
+//                }
+                map.merge(aChar, 1, Integer::sum);
             }
+
         }
         return map;
     }
 
-    private void method(Map<Character, Integer> map) {
+    private List<Map.Entry<Character, Integer>> mapToList(Map<Character, Integer> map) {
         Set<Map.Entry<Character, Integer>> entries = map.entrySet();
-        ArrayList<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(entries);
 
         Comparator<Map.Entry<Character, Integer>> comparator = new Comparator<Map.Entry<Character, Integer>>() {
             @Override
@@ -51,5 +78,6 @@ public class Parsing {
             }
         };
         list.sort(comparator);
+        return list;
     }
 }
