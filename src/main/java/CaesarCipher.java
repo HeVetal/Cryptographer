16d339104911b97cf9723b4ba2730e07b792dbbe
Vetal <heavyve@gmail.com>
2023-08-09 21:48:27 +0300

    в ALPHABET добавлен символ переноса строки

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -2,7 +2,7 @@ public class CaesarCipher {
 
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
                                             "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
-                                            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—" + "0123456789";
+                                            ".,\":!? +-*/\\@#$%^&(){}[];'|`~=_©«»—" + "0123456789" + (char)10 + (char)13;
 
     public int alphabetLength(){
         return ALPHABET.length();
